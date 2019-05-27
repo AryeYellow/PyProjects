8c76971cb5fbb95569e57d832f8f40ef0e524695
洪基伟 <40350987+AryeYellow@users.noreply.github.com>
2019-05-27 10:42:55 +0800

    Add files via upload

--- NLP/NER_Keras_CRF/val.py
+++ NLP/NER_Keras_CRF/val.py
@@ -2,8 +2,8 @@ from numpy import argmax
 from bilsm_crf_model import modeling
 from process_data import pad_seq, chunk_tags
 
-# predict_text = '国务院总理在外交部长陈毅陪同下，访问了埃塞俄比亚'
-predict_text = '国家主席习近平向印尼总统佐科·维多多致贺电，代表中国政府和中国人民，并以个人的名义祝贺他胜选连任印度尼西亚总统。'
+predict_text = '国务院总理在外交部长陈毅陪同下，访问了埃塞俄比亚'
+# predict_text = '国家主席习近平向印尼总统佐科·维多多致贺电，代表中国政府和中国人民，并以个人的名义祝贺他胜选连任印度尼西亚总统。'
 
 model = modeling(train=False)
 x = pad_seq(predict_text)
