41bbc496e459a7482ae4557c241ccf54b2620463
洪基伟 <40350987+AryeYellow@users.noreply.github.com>
2019-05-27 10:40:34 +0800

    Add files via upload

--- NLP/NER_Keras_CRF/bilsm_crf_model.py
+++ NLP/NER_Keras_CRF/bilsm_crf_model.py
@@ -1,34 +1,42 @@
+from os.path import exists
 from keras.models import Sequential
 from keras.layers import Embedding, Bidirectional, LSTM
 from keras_contrib.layers import CRF  # https://github.com/keras-team/keras-contrib
-from process_data import load_data
-import pickle
 from keras.utils.vis_utils import plot_model
 from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from process_data import load_data, chunk_tags, load_vocab  # 自写模块导入
+
+batch_size = 512
+epochs = 20
+verbose = 1
+filepath = 'model/crf.h5'
+min_delta = 1e-9
+patience = 2
+callbacks = [EarlyStopping('val_crf_viterbi_accuracy', min_delta, patience)]
+validation_split = .1
 
 EMBED_DIM = 200
 BiRNN_UNITS = 200
 
 
-def create_model(train=True):
-    if train:
-        (train_x, train_y), (vocab, chunk_tags) = load_data()
-        with open('model/config.pkl', 'wb') as f:
-            pickle.dump((vocab, chunk_tags), f)
-    else:
-        with open('model/config.pkl', 'rb') as f:
-            (vocab, chunk_tags) = pickle.load(f)
-    
+def modeling(train=True):
     model = Sequential()
-    model.add(Embedding(len(vocab), EMBED_DIM, mask_zero=True))  
+    model.add(Embedding(len(load_vocab()) + 1, EMBED_DIM, mask_zero=True))
     model.add(Bidirectional(LSTM(BiRNN_UNITS // 2, return_sequences=True)))
     crf = CRF(len(chunk_tags), sparse_target=True)
     model.add(crf)
     model.summary()
     plot_model(model, to_file='model/model.png', show_shapes=True)
-    model.compile(Adam(.01), loss=crf.loss_function, metrics=[crf.accuracy])
-
+    model.compile(Adam(), loss=crf.loss_function, metrics=[crf.accuracy])
+    if exists(filepath):
+        model.load_weights(filepath)
     if train:
-        return model, (train_x, train_y)
-    else:
-        return model, (vocab, chunk_tags)
+        x, y = load_data()
+        model.fit(x, y, batch_size, epochs, verbose, callbacks, validation_split)
+        model.save_weights(filepath)
+    return model
+
+
+if __name__ == '__main__':
+    modeling()
--- NLP/NER_Keras_CRF/process_data.py
+++ NLP/NER_Keras_CRF/process_data.py
@@ -2,52 +2,67 @@ from collections import Counter
 from keras.preprocessing.sequence import pad_sequences
 import pickle
 import numpy as np
+from os.path import exists
 
-PATH_TRAIN = 'data/train_data.data'
-PATH_TEST = 'data/test_data.data'
+PATH_TRAIN = 'data/train.txt'
+PATH_TEST = 'data/test.txt'
+PATH_VOCAB = 'model/config.pkl'
+PATH_TRAIN_P = 'data/train'
 
-
-def load_data():
-    train = _parse_data()
-    # test = _parse_data(PATH_TEST)
-
-    print(np.array(train).shape)
-    word_counts = Counter(row.lower() for sample in train for row in sample)
-    vocab = [w for w, f in word_counts.most_common() if f >= 2]
-    # B表示开始的字节，I表示中间的字节
-    chunk_tags = ['O', 'B-PER', 'I-PER', 'B-LOC', 'I-LOC', "B-ORG", "I-ORG"]
-
-    # 保存数据
-    with open('model/config.pkl', 'wb') as f:
-        pickle.dump((vocab, chunk_tags), f)
-
-    train = _process_data(train, vocab, chunk_tags)
-    return train, (vocab, chunk_tags)
+# B表示开始的字节，I表示中间的字节
+chunk_tags = ['O', 'B-PER', 'I-PER', 'B-LOC', 'I-LOC', "B-ORG", "I-ORG"]
 
 
 def _parse_data(path=PATH_TRAIN):
     with open(path, encoding='utf-8') as f:
-        return [sample.split() for sample in f.read().split(':')]
+        return [sample.split() for sample in f.read().strip().split('\n\n')]
 
 
-def _process_data(data, vocab, chunk_tags, maxlen=None, onehot=False):
-    if maxlen is None:
-        maxlen = max(len(s) for s in data)
-    word2idx = {w: i for i, w in enumerate(vocab)}
-    x = [[word2idx.get(w.lower(), 1)for w in s[::2]] for s in data]  # 奇数
+def _pad_sequences(data, maxlen=None):
+    maxlen = maxlen or max(len(s) for s in data)
+    word2idx = load_vocab()
+    x = [[word2idx.get(w.lower(), 0)for w in s[::2]] for s in data]  # 奇数
     y_chunk = [[chunk_tags.index(w)for w in s[1::2]] for s in data]  # 偶数
-    x = pad_sequences(x, maxlen)  # left padding
+    # left padding
+    x = pad_sequences(x, maxlen)
     y_chunk = pad_sequences(y_chunk, maxlen, value=-1)
-    if onehot:
-        y_chunk = np.eye(len(chunk_tags), dtype='float32')[y_chunk]
-    else:
-        y_chunk = np.expand_dims(y_chunk, 2)
+    y_chunk = np.expand_dims(y_chunk, 2)
     return x, y_chunk
 
 
-def process_data(data, vocab, maxlen=100):
-    word2idx = {w: i for i, w in enumerate(vocab)}
-    x = [word2idx.get(w[0].lower(), 1) for w in data]
-    length = len(x)
-    x = pad_sequences([x], maxlen)  # left padding
-    return x, length
+def _process_data():
+    train = _parse_data()
+    print(len(train))
+    word_counts = Counter(row.lower() for sample in train for row in sample)
+    vocab = [w for w, f in word_counts.most_common() if f >= 2]
+    word2idx = {w: i for i, w in enumerate(vocab, 1)}
+    print(len(word2idx))
+    # 保存
+    with open(PATH_VOCAB, 'wb') as f:
+        pickle.dump(word2idx, f)
+    x_train, y_train = _pad_sequences(train)
+    with open(PATH_TRAIN_P, 'wb') as f:
+        pickle.dump((x_train, y_train), f)
+
+
+def load_data(path=PATH_TRAIN_P):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+
+def load_vocab():
+    if not exists(PATH_VOCAB):
+        _process_data()
+    with open(PATH_VOCAB, 'rb') as f:
+        return pickle.load(f)
+
+
+def pad_seq(seq):
+    word2idx = load_vocab()
+    x = [word2idx.get(w[0].lower(), 0) for w in seq]
+    x = pad_sequences([x], len(x))
+    return x
+
+
+if __name__ == '__main__':
+    load_vocab()
